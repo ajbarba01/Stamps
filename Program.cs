d1c4e67c004daa6b0b564eeda4da870d054765b7
ajbarba01 <Alexander.Barba@tufts.edu>
2026-04-21 11:20:19 -0400

    new icon

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ internal static class Program
     [STAThread]
     private static void Main()
     {
+        using var mutex = new System.Threading.Mutex(true, "Snip-SingleInstance", out bool isNew);
+        if (!isNew) return;
+
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.SetHighDpiMode(HighDpiMode.SystemAware);
